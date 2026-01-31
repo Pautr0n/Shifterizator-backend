030f58f9b16f59ffc4060baed8f9a5709a593792
PauG <paugaston@hotmail.com>
2026-01-31 20:57:00 +0100

    chore: importing project from spring initializer

---
+++ src/main/java/com/shifterizator/shifterizatorbackend/ShifterizatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shifterizator.shifterizatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifterizatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifterizatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shifterizator/shifterizatorbackend/ShifterizatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shifterizator.shifterizatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifterizatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
