41a2230bfc4737603e8f08b67c4733bd6ccc1c08
PauG <paugaston@hotmail.com>
2026-02-07 00:38:07 +0100

    refactor(employee): changing return types to entity

--- src/main/java/com/shifterizator/shifterizatorbackend/employee/service/PositionService.java
+++ src/main/java/com/shifterizator/shifterizatorbackend/employee/service/PositionService.java
@@ -1,19 +1,19 @@
 package com.shifterizator.shifterizatorbackend.employee.service;
 
-import com.shifterizator.shifterizatorbackend.employee.dto.PositionDto;
+import com.shifterizator.shifterizatorbackend.employee.model.Position;
 
 import java.util.List;
 
 public interface PositionService {
 
-    PositionDto create(String name, Long companyId);
+    Position create(String name, Long companyId);
 
-    PositionDto update(Long id, String name);
+    Position update(Long id, String name);
 
     void delete(Long id);
 
-    PositionDto findById(Long id);
+    Position findById(Long id);
 
-    List<PositionDto> findByCompany(Long companyId);
+    List<Position> findByCompany(Long companyId);
 
 }
