62c9f02a20a09e9597cb98a6235b761f1bfcc822
PauG <paugaston@hotmail.com>
2026-02-07 00:37:52 +0100

    refactor(employee): adding method to PositionRepository

--- src/main/java/com/shifterizator/shifterizatorbackend/employee/repository/PositionRepository.java
+++ src/main/java/com/shifterizator/shifterizatorbackend/employee/repository/PositionRepository.java
@@ -1,10 +1,15 @@
 package com.shifterizator.shifterizatorbackend.employee.repository;
+
 import com.shifterizator.shifterizatorbackend.employee.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
 
     boolean existsByNameAndCompany_Id(String name, Long companyId);
+
+    List<Position> findByCompany_Id(Long companyId);
 }
