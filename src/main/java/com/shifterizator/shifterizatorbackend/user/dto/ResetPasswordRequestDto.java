a5a5996ca77115a8408f86494574271681f17e84
PauG <paugaston@hotmail.com>
2026-02-07 08:57:44 +0100

    feat (user): ResetPasswordRequestDto implemented

---
+++ src/main/java/com/shifterizator/shifterizatorbackend/user/dto/ResetPasswordRequestDto.java
@@ -0,0 +1,18 @@
+package com.shifterizator.shifterizatorbackend.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ResetPasswordRequestDto(
+
+        @NotBlank(message = "New password is required")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).+$",
+                message = "Password must contain lowercase, uppercase, number and special character"
+        )
+        String newPassword
+
+) {
+}
