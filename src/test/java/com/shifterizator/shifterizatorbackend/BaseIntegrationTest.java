bfff012329eeb68f2835d2de9936b282e64ab19f
PauG <paugaston@hotmail.com>
2026-02-10 13:20:18 +0100

    refactor(test): adjusting application-test.yml and BaseIntegrationTest due to testing failures. AuthFlowIt test successfully.

--- src/test/java/com/shifterizator/shifterizatorbackend/BaseIntegrationTest.java
+++ src/test/java/com/shifterizator/shifterizatorbackend/BaseIntegrationTest.java
@@ -3,41 +3,35 @@ package com.shifterizator.shifterizatorbackend;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@ContextConfiguration(initializers = BaseIntegrationTest.Initializer.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseIntegrationTest {
 
-    @Container
-    public static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("shifterizator_test")
-            .withUsername("test")
-            .withPassword("test");
+    private static final org.testcontainers.containers.MySQLContainer<?> mysql =
+            new org.testcontainers.containers.MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("shifterizator_test")
+                    .withUsername("test")
+                    .withPassword("test");
 
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        @Override
-        public void initialize(ConfigurableApplicationContext context) {
-            TestPropertyValues.of(
-                    "spring.datasource.url=" + mysql.getJdbcUrl(),
-                    "spring.datasource.username=" + mysql.getUsername(),
-                    "spring.datasource.password=" + mysql.getPassword(),
-                    "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver"
-            ).applyTo(context.getEnvironment());
-        }
+    static {
+        mysql.start();
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.properties.hibernate.dialect",
+                () -> "org.hibernate.dialect.MySQLDialect");
     }
 
     @Autowired
--- src/test/java/com/shifterizator/shifterizatorbackend/auth/AuthFlowIT.java
+++ src/test/java/com/shifterizator/shifterizatorbackend/auth/AuthFlowIT.java
@@ -6,7 +6,6 @@ import com.shifterizator.shifterizatorbackend.auth.dto.LoginRequestDto;
 import com.shifterizator.shifterizatorbackend.auth.dto.TokenResponseDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -19,8 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class AuthFlowIT extends BaseIntegrationTest {
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     @DisplayName("Superadmin can log in and access a protected endpoint")
